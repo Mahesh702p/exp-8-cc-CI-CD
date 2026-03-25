4f7b12fc4a81c4153926fb491a66049b40f3e93e
Mahesh702p <patelbhiyaram757@gmail.com>
2026-03-25 09:32:26 +0530

    UPDATED g.o.a.t

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "<h1>CI/CD Pipeline is Live!</h1><p>Version 1.0: Initial Deployment.</p>"
+    return "<h1>NAUFIL SIR G.O.A.T</h1><p>Version 1.0: Initial Deployment.</p>"
 
 if __name__ == '__main__':
     application.run(debug=True)
