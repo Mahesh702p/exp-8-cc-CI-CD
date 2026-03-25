1379f66fdbd812fc5bd8b776edc36c5a797f280f
Mahesh702p <patelbhiyaram757@gmail.com>
2026-03-25 09:34:13 +0530

    Update version of goat

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "<h1>NAUFIL SIR G.O.A.T</h1><p>Version 1.0: Initial Deployment.</p>"
+    return "<h1>NAUFIL SIR G.O.A.T</h1><p>Version 2.0: Automated update successfull.</p>"
 
 if __name__ == '__main__':
     application.run(debug=True)
