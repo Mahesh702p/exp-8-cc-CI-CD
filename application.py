951eb7dbdf0a213f54ab754d069c86ee77773d83
Mahesh702p <patelbhiyaram757@gmail.com>
2026-04-17 19:56:46 +0530

    Update greeting message and version in application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "<h1>NAUFIL SIR G.O.A.T</h1><p>Version 1.0: Initial Page.</p>"
+    return "<h1>NAUFIL SIR G.O.T.A</h1><p>Version 2.0: Second Page.</p>"
 
 if __name__ == '__main__':
     application.run(debug=True)
